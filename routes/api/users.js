60c4fffcedd3e9179312c9ff680f116c88ef7085
CustomizedMe <prakharbhatnagar5337@gmail.com>
2020-12-30 13:45:47 +0530

    Prepared for Deployment

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const normalize = require('normalize-url');
+//const normalize = require('normalize-url');
 
 const User = require('../../models/User');
 
