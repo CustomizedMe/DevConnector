f79e6c3024e24857b6c2580c427ed6e1f1bf1402
CustomizedMe <prakharbhatnagar5337@gmail.com>
2021-01-23 16:55:11 +0530

    models updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 
-const User = require("../../models/User");
+const User = require("../../models/user");
 
 // @route    POST api/users
 // @desc     Register user
